34a01bbb9d76ac2b12500b5cf0220cef9b70470e
781115tnt <71996697+781115tnt@users.noreply.github.com>
2026-01-13 14:16:43 +0700

    Add files via upload

---
+++ Pdf-Tif-Png-Pdf/_XuLyHinh.py
@@ -0,0 +1,81 @@
+import subprocess
+from concurrent.futures import ProcessPoolExecutor
+import glob
+import os
+import img2pdf
+from pathlib import Path
+import re
+import psutil
+
+from pypdf import PdfReader
+
+
+
+#Support rotation: 0, 90, 180, 270
+rot = 0
+
+proc_dir = ".\\"
+max_process = psutil.cpu_count(logical=False)
+orig_ext = '.tif'
+out_ext = '.png'
+
+pdf_out_path = Path(proc_dir)
+
+
+def ghostscript_proc(task):
+    filename, pagenumber = task
+    cmd = f"gswin64c.exe -o \".\\{filename}\\{pagenumber}.tif\" -sDEVICE=tiff24nc -sCompression=lzw -r300 -dUseCropBox  -dFirstPage={pagenumber} -dLastPage={pagenumber} \"{filename}\".pdf"
+    print(cmd)
+    return subprocess.run(cmd, shell=True)
+
+def extract_pdf():
+    jobs = []
+    for pdf in Path(proc_dir).glob("*.pdf"):
+        pdf.parent.joinpath(pdf.stem).mkdir(exist_ok=True)
+        for pn in range(1, len(PdfReader(pdf).pages) + 1):
+            jobs.append((pdf.stem, pn))
+
+    with ProcessPoolExecutor(max_workers=max_process) as pool:
+        pool.map(ghostscript_proc, jobs)
+
+def imagemagick_proc(task):
+    inp, out = task
+    if rot == 0 :
+        #cmd = f"convert \"{inp}\" -limit memory 16GiB -level 30%,90% -colors 64 -define png:color-type=3 \"{out}\""
+        cmd = f"convert \"{inp}\" -limit memory 16GiB -fuzz 15%  -fill \"red\"  -opaque \"rgb(220,30,30)\" -fill \"blue\"  -opaque \"rgb(30,30,170)\" -fill \"black\" -opaque \"rgb(20,20,20)\" -fill \"white\" -opaque \"rgb(220,220,220)\"  -level 30%,90% -colors 64  -define png:color-type=3 \"{out}\""
+    else:
+        #cmd = f"convert \"{inp}\" -limit memory 16GiB -level 30%,90% -colors 64 -define png:color-type=3 -rotate {rot} \"{out}\""
+        cmd = f"convert \"{inp}\" -limit memory 16GiB -fuzz 15%  -fill \"red\"  -opaque \"rgb(220,30,30)\" -fill \"blue\"  -opaque \"rgb(30,30,170)\" -fill \"black\" -opaque \"rgb(20,20,20)\" -level 30%,90% -colors 64  -define png:color-type=3 -rotate {rot} \"{out}\""
+        
+    return subprocess.run(cmd, shell=True)
+
+def process_image():
+    all_files = glob.glob(f"{proc_dir}\\**\\*.tif", recursive=True)
+    jobs = []
+    for origimg in all_files:
+        outimg = f"{os.path.splitext(origimg)[0]}{out_ext}"
+        jobs.append((origimg, outimg))  
+    with ProcessPoolExecutor(max_workers=max_process) as pool:
+        pool.map(imagemagick_proc, jobs)
+    
+    print("PROCESSING IMAGES: DONE!!!")
+
+def img2pdf_proc(dir):
+    images = sorted(dir.glob(f"*{out_ext}"))
+    pdf = pdf_out_path.joinpath(f"{re.sub(r"-\d{6}", "", dir.name)}-OUT.pdf")
+    print(f"Creating {pdf}...")
+    with open(pdf, "wb") as f:
+        f.write(img2pdf.convert([str(p) for p in images]))
+
+def create_pdf():
+    subdirs = [x for x in pdf_out_path.iterdir() if x.is_dir()]
+
+    with ProcessPoolExecutor(max_workers=max_process) as pool:
+        pool.map(img2pdf_proc, subdirs)
+
+
+if __name__ == "__main__":   # VERY IMPORTANT ON WINDOWS
+    extract_pdf()
+    process_image() 
+    create_pdf()
+    input("DONE!!!!!!")
