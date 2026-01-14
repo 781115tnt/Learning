0469eae628a82da0d1960e185291bb5ec3952ec1
781115tnt <71996697+781115tnt@users.noreply.github.com>
2026-01-14 23:13:32 +0700

    Add files via upload

--- Pdf-Tif-Png-Pdf/_XuLyHinhV3.py
+++ Pdf-Tif-Png-Pdf/_XuLyHinhV3.py
@@ -10,6 +10,8 @@ import shutil
 from pypdf import PdfReader
 import sys
 from PIL import Image
+#pip install img2pdf psutil pypdf
+
 #For very very large images
 Image.MAX_IMAGE_PIXELS = None
 
@@ -30,33 +32,40 @@ def ghostscript_proc(cmd):
 ########################### MULTIPROCESS-SAFE CODE ############################
 if __name__ == "__main__":   # VERY IMPORTANT ON WINDOWS
 
-    TIF = '.tif'
-    PNG = '.png'
+    TIF = ".tif"
+    PNG = ".png"
     OUTDIR = "_out"
     IM_COLOR_FORMAT = "-colors 64 -define png:color-type=3"
     IM_BW_FORMAT = "-monochrome -compress Group4"
-    GS_24BIT_DEVICE= "-sDEVICE=tiff24nc -sCompression=lzw"
+    IM_PROC_STR = "-fuzz 15% -fill \"red\" -opaque \"rgb(220,30,30)\" -fill \"blue\" -opaque \"rgb(30,30,170)\" -fill \"black\" -opaque \"rgb(20,20,20)\""
+    GS_24BIT_DEVICE= "-sDEVICE=png16m" #"-sDEVICE=tiff24nc -sCompression=lzw"
     GS_1BIT_DEVICE= "-sDEVICE=tiffg4"
-    GS_R600 = '-r600'
-    GS_R300 = '-r300'
-    OUT_FILE_PREFIX = '_OUT_'
+    GS_R600 = "-r600"
+    GS_R300 = "-r300"
+    GS_OUT = "_GS"
+    IM_OUT = "_IM"
+    IGNORED_OUT = "__IGNORE"
+    OUT_FILE_PREFIX = "_OUT_"
+
 
     max_process = psutil.cpu_count(logical=False)
-    wdir = ' '.join(sys.argv[1:])
+    wdir = " ".join(sys.argv[1:])
     wdir_path = Path(wdir)
 
-    out_ext = PNG
+    im_output_ext = PNG
+    gs_output_ext = PNG
     
     gs_resstr = GS_R300
-    gs_cluster = '1'
+    gs_cluster = "1"
     gs_clusterstr = "-dMinFeatureSize=2"
     gs_device = GS_24BIT_DEVICE
 
     im_formatstr = IM_COLOR_FORMAT
-    im_im_rotatestr = ''
-    im_blurstr = ''
+    im_rotatestr = " "
+    im_blurstr = " "
     im_levelstr = "-level 30%,90%"
-    im_rot = '0'
+    im_rot = "0"
+    im_procstr = IM_PROC_STR
 
 
 
@@ -65,22 +74,27 @@ if __name__ == "__main__":   # VERY IMPORTANT ON WINDOWS
     ########################### ASKING FOR OPTIONS ############################
     if not wdir_path.exists():
         input("WORKING DIRECTORY DOES NOT EXIST!!!")
-        sys.exit(0)      
+        sys.exit(0)
+    ignored_path = wdir_path / IGNORED_OUT
+    gsout_path = wdir_path / GS_OUT
+    imout_path = wdir_path / IM_OUT
+
     imgp = input("Khong xu ly dam/nhat/xoa cham khong (Y?)")
-    if imgp == 'y' or imgp == 'Y':
+    if imgp == "y" or imgp == "Y":
         imgProc = False
+        im_procstr = " "
         print("-> Khong thay doi dam/nhat/xoa cham!")
     else:
         imgProc = True
         print("-> Co thay doi dam/nhat/xoa cham!")
 
         im_rot = input("Quay hinh luc xu ly (nhap 90, 180, 270):")
-        if not (im_rot == '90' or im_rot == '180' or im_rot == '270') :
+        if not (im_rot == "90" or im_rot == "180" or im_rot == "270") :
             print("-> Khong quay hinh!")
-            im_im_rotatestr = ''
+            im_rotatestr = " "
         else:
             print(f"-> Quay hinh {im_rot}")
-            im_im_rotatestr = f"-im_rotate {im_rot}"
+            im_rotatestr = f"-im_rotate {im_rot}"
 
         dot = input("Xoa cham (Y?):")
         if dot == "y" or dot == "Y":
@@ -91,28 +105,37 @@ if __name__ == "__main__":   # VERY IMPORTANT ON WINDOWS
             print(f"-> Khong xoa cham!")
 
     bw = input("Ra file 1-bit (Y?):")
-    if bw == 'y' or bw == 'Y':
+    if bw == "y" or bw == "Y":
         print("-> Ra file 1-bit!")
-        out_ext = TIF
+        im_output_ext = TIF
         im_formatstr = IM_BW_FORMAT
         if imgProc:
             gs_device = GS_24BIT_DEVICE
+            gs_output_ext = PNG
         else:
             gs_device = GS_1BIT_DEVICE
+            gs_output_ext = TIF
     else:
-        print(f"-> Ra file mau!")
+        im_output_ext = PNG
+        if imgProc:
+            im_formatstr = IM_COLOR_FORMAT
+            print(f"-> Ra file 64 mau!")
+        else:
+            
+            im_formatstr = " "
+            print(f"-> Ra file 24bit mau!")
     
     res = input("Resolution = 600dpi (Y?):")
-    if res == 'y' or res == 'Y':
+    if res == "y" or res == "Y":
         print("-> Do phan giai 600dpi!")
         gs_resstr = GS_R600
     else:
         print("-> Do phan giai 300dpi!")    
 
     gs_cluster = input("Min Cluster size (nhap 2, 3, 4)?:")
-    if not (gs_cluster == '2' or gs_cluster == '3' or gs_cluster == '4'):
+    if not (gs_cluster == "2" or gs_cluster == "3" or gs_cluster == "4"):
         print("-> Khong set cluster!")
-        gs_clusterstr = ''
+        gs_clusterstr = " "
     else:
         gs_clusterstr = f"-dMinFeatureSize={gs_cluster}"
         print(f"-> Cluster={gs_cluster}!")
@@ -123,56 +146,64 @@ if __name__ == "__main__":   # VERY IMPORTANT ON WINDOWS
     if True:
         for d, _, filenames in os.walk(wdir_path):
             for f in filenames:
+                fnlc = f.lower()
+                if fnlc.endswith(".py") or fnlc.lower().endswith(".bat"): continue
                 full = Path(d) / f
                 flat = "__xxx__".join(full.relative_to(wdir_path).parts)
-                full.rename(wdir_path / flat)
+                full.copy(wdir_path / flat)
+                #full.rename(wdir_path / flat)
 
-        for x in wdir_path.iterdir():
-            if x.is_dir(): shutil.rmtree(x)
+        #for x in wdir_path.iterdir():
+        #    if x.is_dir(): shutil.rmtree(x)
 
+        ignored_path.mkdir(exist_ok=True)
+        input("MOVE CAC FILES KHONG CAN XU LY VAO THU MUC __IGNORE, SAU DO NHAN ENTER DE TIEP TUC...")
 
     ########################### EXTRACTING PDF FILES ############################
     #if False:
     if True:
+        gsout_path.mkdir(exist_ok=True)
         jobs = []
         for pdf in Path(wdir).glob("*.pdf"):
-            pdf.parent.joinpath(pdf.stem).mkdir(exist_ok=True)
+            #Ignore all pdfs in the IGNORED_OUT folder
+            if pdf.parent.name == IGNORED_OUT: continue
+            #Create output folder for each pdf
+            pdfoutpath = (gsout_path / pdf.stem)
+            pdfoutpath.mkdir(exist_ok=True)
+            #Create command for each page
             for pn in range(1, len(PdfReader(pdf).pages) + 1):
-                cmd = f"gswin64c.exe -o \".\\{pdf.stem}\\{pn:010d}.tif\" {gs_device} {gs_resstr} -dUseCropBox {gs_clusterstr} -dFirstPage={pn} -dLastPage={pn} \"{pdf.stem}\".pdf"
+                pnstr = f"{pn:010d}"
+                cmd = f"gswin64c.exe -o \"{pdfoutpath / pnstr}{gs_output_ext}\" {gs_device} {gs_resstr} -dUseCropBox {gs_clusterstr} -dFirstPage={pn} -dLastPage={pn} \"{pdf.stem}\".pdf"
                 jobs.append(cmd)
                 #print(cmd)
         with ProcessPoolExecutor(max_workers=max_process) as pool:
             pool.map(ghostscript_proc, jobs)
 
 
-    ########################### MAKE OUTDIRS ############################
+    ########################### PROCESSING IMAGES ############################
     #if False:
     if True:
-        subdirs = [p for p in wdir_path.glob('**/') if p.is_dir() and p != wdir_path and p.stem != OUTDIR]
-        for sdir in subdirs: os.makedirs(sdir.joinpath(OUTDIR), exist_ok=True)
-
-
-    ########################### PROCESSING IMAGES ############################
-    if imgProc:
-        tfiles = glob.glob(f"{wdir}\\**\\*{TIF}", recursive=False)
-        #print(tfiles)
-        tasks = []
-        for t in tfiles:
-            tpath = Path(t)
-            if tpath.is_dir(): continue
-            ofile = f"{tpath.parent.joinpath(OUTDIR).joinpath(tpath.stem)}{out_ext}"
-            cmd = f"convert \"{t}\" -limit memory 16GiB -fuzz 15%  -fill \"red\" -opaque \"rgb(220,30,30)\" -fill \"blue\" -opaque \"rgb(30,30,170)\" -fill \"black\" -opaque \"rgb(20,20,20)\" {im_im_rotatestr} {im_blurstr} {im_levelstr} {im_formatstr} \"{ofile}\""
-            tasks.append(cmd)  
-        #for t in tasks: print(t)   
-        with ProcessPoolExecutor(max_workers=max_process) as pool: pool.map(imagemagick_proc, tasks)
+        #Create IM_OUT folder and subfolders
+        imout_path.mkdir(exist_ok=True)
+        for p in gsout_path.iterdir():
+            if p.is_dir(): 
+                (imout_path / p.name).mkdir(exist_ok=True)
         
-    else:
-        tfiles = glob.glob(f"{wdir}\\**\\*", recursive=False)
-        for t in tfiles:
-            tpath = Path(t)
-            if tpath.is_dir(): continue
-            ofile = f"{tpath.parent.joinpath(OUTDIR).joinpath(tpath.name)}"
-            tpath.move(ofile)
+        imgs = [f for f in gsout_path.rglob("*") if f.is_file()]
+
+        if imgProc:
+            tasks = []
+            for tpath in imgs:
+                ofile = imout_path / tpath.parent.name / tpath.name
+                cmd = f"magick \"{str(tpath)}\" -limit memory 16GiB {im_procstr} {im_rotatestr} {im_blurstr} {im_levelstr} {im_formatstr} \"{str(ofile)}\""
+                tasks.append(cmd)  
+            #for t in tasks: print(t)   
+            with ProcessPoolExecutor(max_workers=max_process) as pool: pool.map(imagemagick_proc, tasks)
+            
+        else:
+            for tpath in imgs:
+                ofile = imout_path / tpath.parent.name / tpath.name
+                shutil.copy(tpath, ofile)
 
         print("PROCESSING IMAGES: DONE!!!")
 
@@ -180,15 +211,15 @@ if __name__ == "__main__":   # VERY IMPORTANT ON WINDOWS
     ########################### CREATING PDF ############################
     #if False:
     if True:
-        subdirs = [p for p in wdir_path.glob('**/') if p.is_dir() and p != wdir_path and p.stem == OUTDIR]
+        subdirs = [d for d in imout_path.rglob("*") if d.is_dir()]
         tasks = []
         for dir in subdirs:
-            #images = sorted(dir.glob(f"*{out_ext}"))
             images = sorted(dir.glob("*"))
-            pdf = wdir_path.joinpath(f"{OUT_FILE_PREFIX}{re.sub(r"-\d{6}", "", dir.parent.name)}.pdf")
+            pdf = wdir_path / f"{OUT_FILE_PREFIX}{re.sub(r"-\d{6}", "", dir.name)}.pdf"
             tasks.append((pdf, images))
         with ProcessPoolExecutor(max_workers=max_process) as pool:
             pool.map(img2pdf_proc, tasks)
+
 
 
 
